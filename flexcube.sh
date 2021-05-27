dae8c553e3d0c5a38764552efc96a5b6c4bbc6d8
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2021-05-26 21:19:06 -0300

    teste

--- flexcube.sh
+++ flexcube.sh
@@ -1,7 +1,6 @@
-cd /
 su - gsh
-wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/wjZLccY8yvJhr3mRDSeJAdy3JSaDf0M.....HzkgP1AivCU0U4S6aX3DPRJHNw9/n/id3kyspkytmr/b/bucket_banco_conceito/o/kernel144_11Mar21.zip
-
-unzip kernel144_11Mar21.zip
-mv scratch/gsh/kernel144/ /scratch/gsh
+cd /
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/Y86rX7N3n5m39BuMsxkRY-uP5O1ha2ZVEOv-oazTmA6MDf0XNtki8gGymsvYvPEf/n/id3kyspkytmr/b/bucket_banco_conceito/o/kernel144_11Mar21.zip
+unzip -o kernel144_11Mar21.zip -d /
 cd /scratch/gsh/kernel144/user_projects/domains/integrated/bin
+
